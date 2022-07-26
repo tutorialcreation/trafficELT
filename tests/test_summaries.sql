efc590e01b25eabe6b080316553bf982cb1923cb
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-26 10:56:27 +0300

    added time summaries

--- tests/test_summaries.sql
+++ tests/test_summaries.sql
@@ -3,6 +3,9 @@ with distance_summaries as(
 ),
 speed_summaries as(
     select type,max(speed) from endpoints_trafficinfo group by type
+),
+time_summaries as(
+    select type,max(time) from endpoints_trafficinfo group by type
 )
 
 select * from distance_summaries
