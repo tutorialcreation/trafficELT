dae4c3a16e2cde6181f15bcd0de38484e9ad171b
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-26 11:00:35 +0300

    testing speed distributions

--- tests/test_distributions.sql
+++ tests/test_distributions.sql
@@ -2,6 +2,9 @@ with test_distributions as(
     with speed_distribution as (
         select speed,count(*) from endpoints_trafficinfo group by speed
     ),
+    time_distribution as(
+        select time,count(*) from endpoints_trafficinfo group by time
+    )
     
 )
 
