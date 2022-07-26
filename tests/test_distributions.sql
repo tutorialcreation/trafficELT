4fa98ca9cedc051c729d1ec107036fd007b331d7
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-26 10:59:44 +0300

    added speed distributions

--- tests/test_distributions.sql
+++ tests/test_distributions.sql
@@ -1,5 +1,8 @@
 with test_distributions as(
-
+    with speed_distribution as (
+        select speed,count(*) from endpoints_trafficinfo group by speed
+    ),
+    
 )
 
 select * from test_distributions
