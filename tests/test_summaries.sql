e0ba697e3595d4d8038a5bc5c7fc537f2c4e4d83
tutorialcreation <tutorialcreation81@gmail.com>
2022-07-26 10:55:44 +0300

    added speed summaries

--- tests/test_summaries.sql
+++ tests/test_summaries.sql
@@ -1,5 +1,8 @@
 with distance_summaries as(
     select type,max(traveled_d) from endpoints_trafficinfo group by type
+),
+speed_summaries as(
+    select type,max(speed) from endpoints_trafficinfo group by type
 )
 
 select * from distance_summaries
